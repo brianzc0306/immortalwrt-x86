bc066e3b536ea41427f32e7fbcedcc9999512e81
brianzc0306 <48046657+brianzc0306@users.noreply.github.com>
2024-08-22 07:17:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,13 +8,17 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
+
+#Change the default WAN to eth0
+sed -i "s/net\/eth1/net\/eth0/g" package/base-files/files/etc/board.d/99-default_network
+sed -i "s/wan 'eth1'/wan 'eth2'/g" package/base-files/files/etc/board.d/99-default_network
+sed -i "s/lan 'eth0'/lan 'eth1'/g" package/base-files/files/etc/board.d/99-default_network
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
